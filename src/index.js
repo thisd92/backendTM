28fb7d13b40c195266e62c7ade52b81a6da9d7cd
Thiago Dutra <thiiagodutra@gmail.com>
2023-06-30 10:52:17 -0300

    update headers

--- src/index.js
+++ src/index.js
@@ -23,8 +23,9 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://frontend-tm-nu.vercel.app");
+    res.header('Access-Control-Allow-Credentials', true);
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
 });
 
