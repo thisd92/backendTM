03ab0841c46e47f9246f26cc33e63403bc7149f4
Thiago Dutra <thiiagodutra@gmail.com>
2023-06-30 10:17:27 -0300

    update createToken

--- src/index.js
+++ src/index.js
@@ -262,7 +262,7 @@ router.post('/usersLogin', async (req, res, next) => {
         const token = createToken(user);
 
         res.status(200)
-            .cookie('authorization', token, {
+            .cookie('authorization', token.toString(), {
                 httpOnly: false,
                 secure: true,
                 sameSite: 'strict'
